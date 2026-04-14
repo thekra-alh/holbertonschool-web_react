e2825b69919a28e01402a3ee54dc98bd9b913a0f
thekra-alh <202222146@yu.sa.com>
2026-04-14 21:41:54 +0000

    s

---
+++ TypeScript/task_5/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './js/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: '<!doctype html><html><head><meta charset="utf-8"><title>Task 5</title></head><body></body></html>',
+    }),
+  ],
+};
